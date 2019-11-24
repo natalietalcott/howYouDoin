9233d676deb97474d791c64486a42c7ffca08071
Gabby Barbieri <gabbybmeow@gmail.com>
2019-11-24 14:40:03 -0700

    gonna change calendar rather than dom nonsense

--- homePage/calendar.php
+++ homePage/calendar.php
@@ -119,7 +119,8 @@ class Calendar
         }
 
 
-        return '<li id="li-' . $this->currentDate . '" class="' . ($cellNumber % 7 == 1 ? ' start ' : ($cellNumber % 7 == 0 ? ' end ' : ' ')) . ($cellContent == null ? 'mask' : '') . '">' . $cellContent . '</li>';
+        return '<li id="' . $this->currentDate . '" class="' . ($cellNumber % 7 == 1 ? ' start ' : ($cellNumber % 7 == 0 ? ' end ' : ' ')) . ($cellContent == null ? 'mask' : '') . '">' . $cellContent . '</li>';
+        //"li-'
     }
 
     /**
--- homePage/homePage.php
+++ homePage/homePage.php
@@ -2,37 +2,36 @@
 <html>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="UTF-8" />
     <link rel="stylesheet" type="text/css" href="homePage.css" />
     <link href="calendar.css" type="text/css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <title>How You Doin'?</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="homePage.js"></script>
 </head>
-<?php
-include('database_connection.php');
-include 'calendar.php';
-?>
-<?php
-$query = file_get_contents('databaseCreation.sql');
 
-if (mysqli_multi_query($conn, $query)) {
-    // echo "Table created successfully!";
-    do {
-        /* store first result set */
-        if ($result = mysqli_store_result($conn)) {
-            while ($row = mysqli_fetch_row($result)) { }
-            mysqli_free_result($result); // Free in order to store the next
-        }
-    } while (mysqli_next_result($conn));
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-mysqli_next_result($conn);
-?>
 
 <body id="body">
+    <?php
+    include('database_connection.php');
+    include 'calendar.php';
+    $query = file_get_contents('databaseCreation.sql');
+
+    if (mysqli_multi_query($conn, $query)) {
+        // echo "Table created successfully!";
+        do {
+            /* store first result set */
+            if ($result = mysqli_store_result($conn)) {
+                while ($row = mysqli_fetch_row($result)) { }
+                mysqli_free_result($result); // Free in order to store the next
+            }
+        } while (mysqli_next_result($conn));
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_next_result($conn);
+    ?>
     <nav>
         <div class="navbar">
             <a class="log" id="log" href="#"><i class="fa fa-pencil-square-o fa-2x"></i></a>
@@ -62,6 +61,50 @@ mysqli_next_result($conn);
         $calendar = new Calendar();
 
         echo $calendar->show();
+
+        //TOOD: this will be set earlier
+        $email = "gabbybmeow@gmail.com";
+        
+
+        // // create the DOMDocument object
+        // $domDoc = new DOMDocument();
+        // libxml_use_internal_errors(true);
+        // // load content from HTML file
+        // $domDoc->loadHTMLFile(file_get_contents('http://localhost/howYouDoin/homePage/homePage.php'));
+        // libxml_use_internal_errors(true);
+        // if ($domDoc->validate()) {
+        //     echo "This document is valid!\n";
+        // }
+        //get all the calendar dates for this user
+        $logList = array();
+        $query = "SELECT * FROM daily_log";// WHERE email = ?";
+        $stmt = $conn->prepare($query);
+        // $stmt->bind_param("s", $email);
+
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            while ($row = $result->fetch_array()) {
+                array_push($logList, $row);
+            }
+            $stmt->close();
+        }
+        echo "list is:".sizeof($logList);
+
+        // foreach ($logList as $log) {
+        //     echo "LOGS";
+        //     if ($log["note"] !== '') {
+        //         $elem = $domDoc->getElementById($log["date"]);
+        //         $elem->innerHTML = $elem->innerHTML . '<p>~</p>';
+        //     }
+        //     if ($log["emotion"] !== '') {
+        //         echo"date:".$log["date"];
+        //         $elem = $domDoc->getElementById($log["date"]);
+        //         $elem->setAttribute('class', $log["emotion"]);
+        //     }
+        // }
+
+        // // print document (with changes)
+        // echo $domDoc->saveXML();
         ?>
         <!-- <div style="display: flex;">
             <div class="calendar" id="november">
