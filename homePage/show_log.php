6e9b90654de9512d05cbd50eeeab143fb9bd87f7
Madeline McKune <madelinemckune@mymail.mines.edu>
2019-12-04 20:49:09 -0700

    update and delete works

--- homePage/show_log.php
+++ homePage/show_log.php
@@ -10,10 +10,32 @@
 </head>
 
 <body>
-	<?php
-	//try to connect to the backend so we can verify the login
-    include('../homePage/database_connection.php');
-    $query = file_get_contents('../homePage/databaseCreation.sql');
+<?php
+    $cookie_name = "loginCredentials";
+    if (!isset($_COOKIE[$cookie_name])) {
+        // Not logged in :(
+        header("Location: ../login/login.php"); /* Redirect browser */
+        exit();
+        // echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+        // Logged In
+        // echo "Cookie '" . $cookie_name . "' is set!<br>";
+        // echo "Value is: " . $_COOKIE[$cookie_name];
+
+        // //send an email
+        // $to = $_COOKIE[$cookie_name];
+        // $subject = "Test Subject";
+        // $msg = "You logged into my website!!\nSuper neat! :))";
+        // // use wordwrap() if lines are longer than 70 characters
+        // // $msg = wordwrap($msg, 70);
+        // $headers = "From: gabbybmeow@gmail.com" . "\r\n"; 
+        // //send email
+        // mail($to, $subject, $msg, $headers);
+    }
+
+    include('database_connection.php');
+    include 'calendar.php';
+    $query = file_get_contents('databaseCreation.sql');
 
     if (mysqli_multi_query($conn, $query)) {
         // echo "Table created successfully!";
@@ -27,64 +49,89 @@
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
-	mysqli_next_result($conn);
+    mysqli_next_result($conn);
     ?>
 	<?php
 		//set the variables
-		//$email = $password = '';
+        $email = $_COOKIE[$cookie_name];
+        $date =$_GET['date'];
+        $selectedEmotion = $note = $tag = '';
 	?>
 	<?php 
 	 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-         /*
-		if (isset($_POST['email'])){
-			$email = $_POST['email'];
-			//echo ($email);
-		}
-		if (isset($_POST['password'])){
-			$password = $_POST['password'];
-			//echo($password);
-		}
-		if (isset($_POST['submit'])) {
-			$query = "SELECT * FROM USER_ACCOUNT WHERE (email = ? && password = ?)";
-			$stmt = $conn->prepare($query);
-            $stmt->bind_param("ss", $email, $password);
+         if (isset($_POST['delete'])){
+            $query = "DELETE FROM DAILY_LOG WHERE (email = ? && date = ?)";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("ss", $email, $date);
             if ($stmt->execute()) {
-				$count = 0;
                 $result = $stmt->get_result();
-                while ($row = $result->fetch_array()) {
-                    $count++;
-				}
-				$stmt->close();
-				if ($count > 0){
-					header('Location: ../homePage/homePage.php');
-				} else {
-					echo("this echo does not exist");
-				}
+            } else {
+                echo ("ERROR");
             }
-            else {
-                echo $stmt -> error;
+         }
+         if (isset($_POST['update'])){
+             
+            if (isset($_POST['note'])) {
+                $note = ($_POST['note']);
             }
-        }
-        */
+            
+            if (isset($_POST['daily_tag'])) {
+                $tag = ($_POST['daily_tag']);
+            }
+            
+            if (isset($_POST['emotion'])) {
+                $selectedEmotion = $_POST['emotion'];
+                //echo($selected_emotion);
+            }
+            $query = "UPDATE DAILY_LOG SET tag=?, note=?, emotion=?  WHERE (email = ? && date = ?)";
+            $stmt = $conn->prepare($query);
+            $stmt->bind_param("sssss", $tag, $note, $emotion, $email, $date);
+            if ($stmt->execute()) {
+                $result = $stmt->get_result();
+            } else {
+                echo ("ERROR");
+            }
+         }
 	}
 	?>
+    <?php
+        //get the value of the calendar block
+        $query = "SELECT * FROM DAILY_LOG WHERE (email = ? && date = ?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ss", $email, $date);
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+            //echo($result);
+            while ($row = $result->fetch_array()) {
+                //echo($row['email']);
+                $selectedEmotion = $row['emotion'];
+                $tag = $row['tag'];
+                $note = $row['note'];
+            }
+        } else {
+            echo ("ERROR");
+        }
+
+    ?>
 	<header>
 		<section id="log">
 			<h1> View & Update Log </h1>
 			<!-- <h2> Log In </h2> -->
 			<div id="formWrap">
 				<form method="post">
-                    <label for="date" id="date_label">DATE SUBMITTED</label>
-					<p id="emotion">SELECTED EMOTION</p>
-                    <select name="daily_tag" value="neutral">
+                    <label for="date" id="date_label">DATE: <?php echo $date ?></label>
+					<p id="emotion">EMOTION: <?php echo $selectedEmotion;?></p>
+                    <select name="emotion">
+                        <option value = ""> Change Emotion </option>
                         <option value="super-sad">Super Sad</option>
                         <option value="sad">Sad</option>
                         <option value="neutral">Neurtral</option>
                         <option value="happy">Happy</option>
                         <option value="super-happy">Super Happy</option>
                     </select>
-                    <p id="tag">SELECTED TAG</p>
-                    <select name="daily_tag" value="family">
+                    <p id="tag">TAG: <?php echo $tag;?></p>
+                    <select name="daily_tag">
+                        <option value= "">Change Tag</option>
                         <option value="weather">Weather</option>
                         <option value="friends">Friends</option>
                         <option value="family">Family</option>
@@ -93,10 +140,10 @@
                         <option value="drama">Drama</option>
                         <option value="relationship">Relationship</option>
                     </select>
-                    <p id="note">REASONING</p>
-                    <textarea rows="5" type="text" name="note" class="input_note" value="<?php echo $note; ?>"></textarea><br>
-                    <input id="submit" type="submit" value="Update" name="submit">
-                    <input id="submit" type="submit" value="Delete" name="submit"><br><br>
+                    <p id="note">REASONING: </p> 
+                    <textarea rows="5" type="text" name="note" class="input_note"> <?php echo $note; ?></textarea><br>
+                    <input id="submit" type="submit" value="Update" name="update">
+                    <input id="submit" type="submit" value="Delete" name="delete"><br><br>
 				</form>
             </div>
             <!--
